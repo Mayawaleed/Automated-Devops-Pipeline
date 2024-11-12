bd6b111aaa67b31a5606cfa724392c802a93127d
Mayawaleed <125691425+Mayawaleed@users.noreply.github.com>
2024-11-12 22:19:44 +0200

    dashborad css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import styles from '../styles/NavBar.module.css';
 
+
 const NavBar = () => {
     return (
       <nav className={styles.navbar}>
