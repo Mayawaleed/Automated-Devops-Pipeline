455357b91b80e6c70338a800cacf3e3dabbf86a4
Mayawaleed <125691425+Mayawaleed@users.noreply.github.com>
2024-11-12 22:10:51 +0200

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,18 +7,23 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>DevOps</h1>
+        <h1 className={styles.title}>DevOps </h1>
+
         <h1 className={styles.title}>Engineer</h1>
+        <p className={styles.subtitle}>
+          Empowering your infrastructure with automation, tools, and scalable solutions.
+        </p>
         <div className={styles.buttonContainer}>
-          <Link href="/about">
-            <button className={styles.learnMoreButton}>Learn More</button>
-          </Link>
+<Link href="/learn-more">
+  <button className={styles.learnMoreButton}>Learn More</button>
+</Link>
+
         </div>
       </div>
 
       <div className={styles.imageContainer}>
         <Image
-          src="/images/devops.jpeg"  // Correct path to the 'public' folder
+          src="/images/devops.jpeg" // Correct path to the 'public' folder
           alt="DevOps Image"
           width={600}  // Width of the image
           height={400} // Height of the image
