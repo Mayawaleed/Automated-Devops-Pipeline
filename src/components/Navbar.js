f2f7267bd036bad0b503e05d751230965dcd15d5
esraa <147067443+EsraaHassann@users.noreply.github.com>
2024-11-12 16:07:50 +0200

    home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,25 +3,17 @@ import Link from 'next/link';
 import styles from '../styles/NavBar.module.css';
 
 const NavBar = () => {
-  return (
-    <nav className={styles.navbar}>
-      
-      <ul className={styles.navLinks}>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/create-project">Create Project</Link>
-        </li>
-        <li>
-          <Link href="/about">About Us</Link>
-        </li>
-      </ul>
-      <div className={styles.getStarted}>
-        <Link href="/get-started">Get Started</Link>
-      </div>
-    </nav>
-  );
-};
+    return (
+      <nav className={styles.navbar}>
+       
+        <ul className={styles.navbarLinks}>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/create-project">Create Project</Link></li>
+          <li><Link href="/about">About Us</Link></li>
+        </ul>
+        <button className={styles.navbarButton}>Get Started</button>
+      </nav>
+    );
+  };
 
 export default NavBar;
