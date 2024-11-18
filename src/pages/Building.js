497bed0969ad11d2ff74d904d3caadd4bfb9acc7
esraa <147067443+EsraaHassann@users.noreply.github.com>
2024-11-18 16:22:36 +0200

    building js

---
+++ src/pages/Building.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Stage.module.css';
+
+const Building = () => {
+  return (
+    <div className={styles.stageContainer}>
+      <h1>Building Stage</h1>
+      <p>This is the Building stage of the DevOps lifecycle. Content goes here...</p>
+    </div>
+  );
+};
+
+export default Building;
