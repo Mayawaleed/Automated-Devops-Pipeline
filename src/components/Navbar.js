7f91363c9933dbdc501da6e37c532ef5211b9b44
ayaehabb <aya2103436@miuegypt.edu.eg>
2024-11-18 16:44:24 +0200

    monitoring tool

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,46 @@
+// import React from 'react';
+// import Link from 'next/link';
+// import styles from '../styles/Navbar.module.css';
+
+// const NavBar = () => {
+//     return (
+//       <nav className={styles.navbar}>
+       
+//         <ul className={styles.navbarLinks}>
+//           <li><Link href="/">Home</Link></li>
+//           <li><Link href="/createProject">Create Project</Link></li>
+//           <li><Link href="/dashboard">Dashboard</Link></li>
+//           <li><Link href="/about">About Us</Link></li>
+//         </ul>
+//         <Link href="/signin">
+//         <button className={styles.navbarButton}>Get Started</button>
+//       </Link>
+       
+//       </nav>
+//     );
+//   };
+
+// export default NavBar;
+
 import React from 'react';
 import Link from 'next/link';
 import styles from '../styles/Navbar.module.css';
 
 const NavBar = () => {
     return (
-      <nav className={styles.navbar}>
-       
-        <ul className={styles.navbarLinks}>
-          <li><Link href="/">Home</Link></li>
-          <li><Link href="/createProject">Create Project</Link></li>
-          <li><Link href="/dashboard">Dashboard</Link></li>
-          <li><Link href="/about">About Us</Link></li>
-        </ul>
-        <Link href="/signin">
-        <button className={styles.navbarButton}>Get Started</button>
-      </Link>
-       
-      </nav>
+        <nav className={styles.navbar}>
+            <ul className={styles.navbarLinks}>
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/createProject">Create Project</Link></li>
+                <li><Link href="/dashboard">Dashboard</Link></li>
+                <li><Link href="/about">About Us</Link></li>
+                <li><Link href="/downloadMonitoringTool">Scalling</Link></li> {/* Added this line */}
+            </ul>
+            <Link href="/signin">
+                <button className={styles.navbarButton}>Get Started</button>
+            </Link>
+        </nav>
     );
-  };
+};
 
 export default NavBar;
