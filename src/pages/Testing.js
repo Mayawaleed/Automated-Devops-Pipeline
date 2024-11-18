d2cb96125044fa683a5324d580639d0c91754c5e
esraa <147067443+EsraaHassann@users.noreply.github.com>
2024-11-18 16:22:07 +0200

    testing js

---
+++ src/pages/Testing.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Stage.module.css';
+
+const Testing = () => {
+  return (
+    <div className={styles.stageContainer}>
+      <h1>Testing Stage</h1>
+      <p>This is the Testing stage of the DevOps lifecycle. Content goes here...</p>
+    </div>
+  );
+};
+
+export default Testing;
