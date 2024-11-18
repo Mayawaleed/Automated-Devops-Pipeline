4d339f39ddc362ff1ea8013cbc38302d569bdc68
esraa <147067443+EsraaHassann@users.noreply.github.com>
2024-11-18 15:49:16 +0200

    footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,3 @@
-// src/components/Footer.js
 import styles from '../styles/Footer.module.css';
 import Link from 'next/link';
 
@@ -6,13 +5,22 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
-        <p>&copy; {new Date().getFullYear()} DevOps Optimizer. All Rights Reserved.</p>
-        <div className={styles.footerLinks}>
-          <Link href="/about">About</Link>
-          <Link href="/contact">Contact</Link>
-          <Link href="/privacy">Privacy Policy</Link>
+        <div className={styles.facultyLinks}>
+          <h3>Faculties Links</h3>
+          <ul>
+            <li><Link href="/alsun">Alsun</Link></li>
+            <li><Link href="/mass-communication">Mass Communication</Link></li>
+            <li><Link href="/oral-dental-medicine">Oral & Dental Medicine</Link></li>
+            <li><Link href="/engineering-architecture">Engineering - Architecture</Link></li>
+            <li><Link href="/computer-science">Computer Science</Link></li>
+            <li><Link href="/business-administration">Business Administration</Link></li>
+            <li><Link href="/pharmacy">Pharmacy</Link></li>
+            <li><Link href="/engineering-electronics">Engineering - Electronics</Link></li>
+          </ul>
         </div>
       </div>
+      <p>&copy; {new Date().getFullYear()} DevOps Optimizer. All Rights Reserved.</p>
+
     </footer>
   );
 };
