ffae367a1a5b6f180a315e51e8cc5b91d08521b4
esraa <147067443+EsraaHassann@users.noreply.github.com>
2024-11-18 16:22:45 +0200

    planning js

---
+++ src/pages/Planning.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Stage.module.css';
+
+const Planning = () => {
+  return (
+    <div className={styles.stageContainer}>
+      <h1>Planning Stage</h1>
+      <p>This is the Planning stage of the DevOps lifecycle. Content goes here...</p>
+    </div>
+  );
+};
+
+export default Planning;
