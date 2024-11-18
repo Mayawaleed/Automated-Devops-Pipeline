7fb1b8f6eb2ccbb0cfbb068bc165a7e4a8a370dc
ayaehabb <aya2103436@miuegypt.edu.eg>
2024-11-18 16:45:09 +0200

    edit

---
+++ src/pages/downloadMonitoringTool.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from '../styles/Download.module.css';
+
+const DownloadMonitoringTool = () => {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Download Monitoring Tool</h1>
+            <p className={styles.description}>
+               download our powerful tool
+            </p>
+            <div className={styles.buttonContainer}>
+                <a
+                    // href="/path/to/monitoring-tool.zip" // Replace with actual file path
+                    download
+                    className={styles.downloadButton}
+                >
+                    Download Now
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default DownloadMonitoringTool;
