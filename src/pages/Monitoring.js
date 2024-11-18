00f2b9164fef5b2aa92a30aee79ddb738f40abe9
esraa <147067443+EsraaHassann@users.noreply.github.com>
2024-11-18 16:22:16 +0200

    testing js

---
+++ src/pages/Monitoring.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Stage.module.css';
+
+const Monitoring = () => {
+  return (
+    <div className={styles.stageContainer}>
+      <h1>Monitoring Stage</h1>
+      <p>This is the Monitoring stage of the DevOps lifecycle. Content goes here...</p>
+    </div>
+  );
+};
+
+export default Monitoring;
