ddd1a6e09425e770b8f386fcb902bb6b10117905
esraa <147067443+EsraaHassann@users.noreply.github.com>
2024-11-18 16:22:26 +0200

    deployment js

---
+++ src/pages/Deployment.js
@@ -0,0 +1,12 @@
+import styles from '../styles/Stage.module.css';
+
+const Deployment = () => {
+  return (
+    <div className={styles.stageContainer}>
+      <h1>Deployment Stage</h1>
+      <p>This is the Deployment stage of the DevOps lifecycle. Content goes here...</p>
+    </div>
+  );
+};
+
+export default Deployment;
