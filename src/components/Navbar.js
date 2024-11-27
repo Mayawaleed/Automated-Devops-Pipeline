ce3a70a0f76624852160d028f42859c5a2717089
esraa <147067443+EsraaHassann@users.noreply.github.com>
2024-11-27 15:55:29 +0200

    .

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -34,7 +34,7 @@ const NavBar = () => {
                 <li><Link href="/createProject">Create Project</Link></li>
                 <li><Link href="/dashboard">Dashboard</Link></li>
                 <li><Link href="/about">About Us</Link></li>
-                <li><Link href="/downloadMonitoringTool">Scalling</Link></li> {/* Added this line */}
+                <li><Link href="/downloadMonitoringTool">Scaling</Link></li> {/* Added this line */}
             </ul>
             <Link href="/signin">
                 <button className={styles.navbarButton}>Get Started</button>
