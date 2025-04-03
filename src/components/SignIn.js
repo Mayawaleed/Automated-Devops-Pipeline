c1130ca6e61781489a2047a25cffb017f6daa123
esraa <147067443+EsraaHassann@users.noreply.github.com>
2025-04-03 02:35:23 +0200

    push

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -365,7 +365,7 @@ const SignIn = () => {
         }
 
         try {
-            const response = await axios.post('http://localhost:8000/api/login/', {
+            const response = await axios.post('http://127.0.0.1:8000/api/login/', {
                 username,
                 password
             });
